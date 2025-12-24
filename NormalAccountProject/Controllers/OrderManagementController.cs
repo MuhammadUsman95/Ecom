74c8f667483f7b1527ff63adbb1f95e26da62113
AshrafShakeel <ashraf.shakeel@gentecsoft.com>
2025-12-25 00:12:09 +0500

    Create OrderManagementController.cs

---
+++ NormalAccountProject/Controllers/OrderManagementController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NormalAccountProject.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Net;
+using static NormalAccountProject.Controllers.DashboardController;
+
+namespace NormalAccountProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderManagementController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private string connectionString;
+
+        public OrderManagementController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("Connection1");
+        }
+
+        [HttpPost("nLoadCustomerRegistrationData")]
+        public async Task<IActionResult> nLoadCustomerRegistrationData([FromBody] nInfoTab nInfoTabObj)
+        {
+            try
+            {
+
+               
+                var response = new
+                {
+                    statusId = 1
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    statusId = 0,
+                    message = "Error: " + ex.Message
+                });
+            }
+        }
+        
+        [HttpPost("nLoadGridViewData")]
+        public async Task<IActionResult> nLoadGridViewData([FromBody] nInfoTab nInfoTabObj)
+        {
+            try
+            {
+
+                var parameters = new Dictionary<string, object>
+                {
+                    { "@nType", 0 },
+                    { "@nsType", 2 }
+                };
+
+                List<ExpandoObject> nDataList = await nGetDataAsync<ExpandoObject>("Customer_SP", parameters);
+
+                var response = new
+                {
+                    statusId = 1,
+                    GridViewDataList = nDataList
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    statusId = 0,
+                    message = "Error: " + ex.Message
+                });
+            }
+        }
+
+
+        public async Task<List<T>> nGetDataAsync<T>(string storedProcedure, Dictionary<string, object> parameters) where T : new()
+        {
+            List<T> list = new();
+
+            using SqlConnection con = new SqlConnection(connectionString);
+            using SqlCommand cmd = new SqlCommand(storedProcedure, con);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            if (parameters != null)
+            {
+                foreach (var param in parameters)
+                    cmd.Parameters.AddWithValue(param.Key, param.Value ?? DBNull.Value);
+            }
+
+            await con.OpenAsync();
+
+            using SqlDataReader dr = await cmd.ExecuteReaderAsync();
+
+            if (typeof(T) == typeof(ExpandoObject))
+            {
+                // ExpandoObject handling
+                while (await dr.ReadAsync())
+                {
+                    IDictionary<string, object> expando = new ExpandoObject();
+
+                    for (int i = 0; i < dr.FieldCount; i++)
+                    {
+                        expando[dr.GetName(i)] = dr.IsDBNull(i) ? null : dr.GetValue(i);
+                    }
+
+                    list.Add((T)expando);
+                }
+            }
+            else
+            {
+                // Normal class handling via reflection
+                var props = typeof(T).GetProperties();
+
+                while (await dr.ReadAsync())
+                {
+                    T obj = new T();
+
+                    foreach (var prop in props)
+                    {
+                        if (!dr.HasColumn(prop.Name) || dr[prop.Name] == DBNull.Value)
+                            continue;
+
+                        prop.SetValue(obj, Convert.ChangeType(dr[prop.Name], prop.PropertyType));
+                    }
+
+                    list.Add(obj);
+                }
+            }
+
+            return list;
+        }
+    }
+}
