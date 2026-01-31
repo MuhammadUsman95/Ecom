59ae093f43c4b5cd70f22aff7711e3fc78d9b22d
MuhammadUsman95 <75527933+MuhammadUsman95@users.noreply.github.com>
2026-01-31 19:51:55 +0500

    Usman

--- NormalAccountProject/wwwroot/js/main-vanilla.js
+++ NormalAccountProject/wwwroot/js/main-vanilla.js
@@ -476,7 +476,7 @@ ready(() => {
         <div style="
           width: 100%; 
           height: 284px; 
-          background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+          background: linear-gradient(135deg, #2d2e31 0%, #68646d 100%);
           display: flex;
           align-items: center;
           justify-content: center;
