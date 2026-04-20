61fcda750deee1ae542a9c4390d6356856423345
MuhammadUsman95 <75527933+MuhammadUsman95@users.noreply.github.com>
2026-04-20 23:40:27 +0500

    Usman

---
+++ NormalAccountProject/Pages/PurchaseInvoice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NormalAccountProject.Pages
+{
+    public class PurchaseInvoiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
